3a1332b656528d39c06966a35a74927da893f97d
leo.hecl <leo.hecl@ef.com>
2015-01-16 15:48:49 +0800

    s

--- repoSetting.js
+++ repoSetting.js
@@ -12,15 +12,17 @@ module.exports = {
 			'labs-specialdeal' : ['rio2016languagetest', /*'login-handler', 'russ-ui-headerfooter'*/]
 		},
 	updateRepo:{
-		'labs-share' : ['service-clients', 'shared-services', 
+		'labs-share' : ['service-clients', 'shared-services'/*, 
 						{'enrollment': 
 							{branch:'team/specialdeals/develop'}
-						}
+						}*/
 					],
 		'labs-commerce' : [],
 		'labs-mobile' : [],
 		'labs-ops' : [],
-		'labs-school' : [],
+		'labs-school' : [{'school-platform':{legacy:true}}, 
+						{'school' : {legacy:true, branch:'team/specialdeals/develop'}}
+		],
 		'labs-solution' : [],
 		'labs-specialdeal' : []
 	}
